3159c04c2ef182430d70e870710ea934c4c68bff
marfa <hw2023use@gmail.com>
2024-08-04 22:49:08 +0300

    feat: create function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let str = n.toString();
+  let reverseStr = '';
+
+  for (let i = (str.length -1 ); i >= 0; i--) {
+    reverseStr += str[i];
+  }
+
+  return +reverseStr;
 }
